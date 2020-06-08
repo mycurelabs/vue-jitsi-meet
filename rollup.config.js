bd683261a84200f2469ea61062439c1da64bd15d
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-06-08 15:51:05 +0800

    chore: create rollup config

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+import babel from 'rollup-plugin-babel';
+import vue from 'rollup-plugin-vue';
+import postcss from 'rollup-plugin-postcss';
+import { terser } from 'rollup-plugin-terser';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      exports: 'named',
+      name: 'vue-jitsi-meet',
+      file: 'dist/index.js',
+      format: 'cjs'
+    },
+    {
+      exports: 'named',
+      name: 'VueJitsiMeet',
+      file: 'dist/vue-jitsi-meet.js',
+      format: 'umd'
+    }
+  ],
+  plugins: [ 
+    terser(),
+    vue(),
+    resolve(),
+    babel({
+      runtimeHelpers: true,
+      exclude: /node_modules/
+    }),
+    postcss({
+      plugins: []
+    }),
+    commonjs()
+  ]
+};
